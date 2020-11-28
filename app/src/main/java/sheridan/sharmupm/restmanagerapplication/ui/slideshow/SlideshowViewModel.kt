8d032b5ad9a85f029afef891ac15c83dca98c4dc
Upma <Upma.Sharma@ontario.ca>
2020-11-28 14:26:30 -0500

    Master Branch Initial Commit

---
+++ app/src/main/java/sheridan/sharmupm/restmanagerapplication/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package sheridan.sharmupm.restmanagerapplication.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
