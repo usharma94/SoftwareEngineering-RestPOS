c975451ac2cee4bd76585d6890ecf46c97eb9243
Upma <Upma.Sharma@ontario.ca>
2020-12-16 14:00:47 -0500

    HOME PAGE AND BASIC GALLERY NAVIGATION TEST AND SETUP

--- app/src/main/java/sheridan/sharmupm/restmanagerapplication/presentation/home/HomeFragment.kt
+++ app/src/main/java/sheridan/sharmupm/restmanagerapplication/presentation/home/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 import sheridan.sharmupm.restmanagerapplication.R
 
 class HomeFragment : Fragment() {
@@ -22,10 +24,23 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        //val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+           // textView.text = it
+           button.setOnClickListener{showInput()}
+            menu_card.setOnClickListener{showInput()}
+
         })
+        //button.setOnClickListener{showInput()}
+
+
+
         return root
     }
+
+    private fun showInput() {
+        this.findNavController().navigate(R.id.action_nav_home_to_nav_gallery)
+
+    }
+
 }
