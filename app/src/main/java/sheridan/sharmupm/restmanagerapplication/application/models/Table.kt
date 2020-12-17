29ffba204393ed2baf474c4a8797ca615009862a
Upma <Upma.Sharma@ontario.ca>
2020-12-17 13:00:37 -0500

    Table model update

--- app/src/main/java/sheridan/sharmupm/restmanagerapplication/application/models/Table.kt
+++ app/src/main/java/sheridan/sharmupm/restmanagerapplication/application/models/Table.kt
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.IgnoreExtraProperties
 data class Table(
     var numOfSeats: Int? = null,
     var tableOrders: List<String>? = null,
+    var available : Boolean = true,
 ) {
     companion object {
         const val FIELD_NUMOFSEATS = "numOfSeats"
